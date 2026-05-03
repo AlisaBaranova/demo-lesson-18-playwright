67988eb1c9ec3e8b7a7889672264f8b417fd80bb
Alisa Baranova <aliskab@inbox.lv>
2026-05-03 23:13:01 +0300

    feat/ab/TL-18

--- tests/ui/e2e-flow-with-before.spec.ts
+++ tests/ui/e2e-flow-with-before.spec.ts
@@ -13,19 +13,19 @@ test.beforeEach(async ({ page }) => {
 test('signIn button disabled when incorrect data inserted', async ({}) => {
   await loginPage.usernameField.fill(faker.lorem.word(2))
   await loginPage.passwordField.fill(faker.lorem.word(7))
-  await expect(loginPage.signInButton).toBeDisabled()
+  await loginPage.signInButton.checkEnabled(false)
 })
 
-test('error message displayed when incorrect credentials used', async ({}) => {
-  // implement test
-})
+// test('error message displayed when incorrect credentials used', async ({}) => {
+//   // implement test
+// })
 
 test('login with correct credentials and verify order creation page', async ({}) => {
   const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD)
-  await expect(orderCreationPage.statusButton).toBeVisible()
+  await orderCreationPage.statusButton.checkVisible(true)
   // verify at least few elements on the order creation page
 })
 
-test('login and create order', async ({}) => {
-  // implement test
-})
+// test('login and create order', async ({}) => {
+//   // implement test
+// })
